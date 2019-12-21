5c88ee1947c4c2c9b95c50afca163427a055b083
vkhorozian <vkhorozian@gmail.com>
2019-12-21 20:57:06 +0000

    first Commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+/* Storing multi-line JSON string in a JS variable
+using the new ES6 template literals
+var json = `{
+    "book": {
+        "name": "Harry Potter and the Goblet of Fire",
+        "author": "J. K. Rowling",
+        "year": 2000,
+        "characters": ["Harry Potter", "Hermione Granger", "Ron Weasley"],
+        "genre": "Fantasy Fiction",
+        "price": {
+            "paperback": "$10.40", "hardcover": "$20.32", "kindle": "$4.11"
+        }
+    }
+}`;
+
+// Converting JSON object to JS object
+var obj = JSON.parse(json);
+
+// Define recursive function to print nested values
+function printValues(obj) {
+    for(var k in obj) {
+        if(obj[k] instanceof Object) {
+            printValues(obj[k]);
+        } else {
+            document.write(obj[k] + "<br>");
+        };
+    }
+};
+
+// Printing all the values from the resulting object
+printValues(obj);
+
+document.write("<hr>");
+
+// Printing a single value
+document.write(obj["book"]["author"] + "<br>");  // Prints: J. K. Rowling
+document.write(obj["book"]["characters"][0] + "<br>");  // Prints: Harry Potter
+document.write(obj["book"]["price"]["hardcover"]);  // Prints: $20.32
+*/
+document.write(["response"]["games"]["playtime_forever"] + "<br>");
